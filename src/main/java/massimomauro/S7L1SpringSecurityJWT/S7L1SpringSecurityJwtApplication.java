eb1a313dba6a4d7b1047a0da658f3e130859ce94
MrMax01 <massimo.mauro00@gmail.com>
2023-11-13 15:16:40 +0100

    first commit

---
+++ src/main/java/massimomauro/S7L1SpringSecurityJWT/S7L1SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S7L1SpringSecurityJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L1SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L1SpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S7L1SpringSecurityJWT/S7L1SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S7L1SpringSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L1SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
